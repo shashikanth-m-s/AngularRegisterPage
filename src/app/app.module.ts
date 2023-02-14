f4bf46ac394a6bb70ccc9582a984e1bdad7de442
Shashikanth M S <shashikanthms1998@gmail.com>
2023-02-14 20:38:04 +0530

    register page angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserregisterComponent } from './userregister/userregister.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserregisterComponent
   ],
   imports: [
     BrowserModule,
